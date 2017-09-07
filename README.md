0738eaddae32dfe401bb6a967a364ce7d479bbff
Peter Jaszkowiak <p.jaszkow@gmail.com>
2017-09-06 20:42:20 -0600

    Init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vital Emoji for NodeBB
+
+This emoji pack concentrates commonly used, non-standard emoji from Github and elsewhere for use in NodeBB.
+
+The emoji images are unlicensed, use at your own risk.
+
+## Installation
+
+This pack requires `nodebb-plugin-emoji` version 2. For best results, install and activate both plugins through your Admin Control Panel in NodeBB.
+
+For manual installation, `npm install nodebb-plugin-emoji nodebb-plugin-emoji-vital`.
---
+++ emoji.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (callback) => {
+  fs.readFile(path.join(__dirname, 'emoji/LICENSE'), (err, buffer) => {
+    if (err) {
+      return callback(err);
+    }
+
+    const license = buffer.toString();
+    
+    callback(null, {
+      name: 'Vital Emoji',
+      id: 'vital',
+      attribution: '',
+      license,
+      mode: 'images',
+      images: {
+        directory: path.join(__dirname, 'emoji'),
+      },
+      dictionary: {
+        trollface: {
+          aliases: ['troll'],
+          image: 'trollface.png',
+          character: '',
+        },
+        squirrel: {
+          aliases: ['shipit'],
+          image: 'squirrel.png',
+          character: '🐿️',
+        },
+        octocat: {
+          aliases: ['github'],
+          image: 'octocat.png',
+          character: '',
+        },
+        feelsgood: {
+          aliases: [],
+          keywords: ['doom', 'space', 'marine'],
+          image: 'feelsgood.png',
+          character: '',
+        },
+        finnadie: {
+          aliases: [],
+          keywords: ['doom', 'space', 'marine'],
+          image: 'finnadie.png',
+          character: '',
+        },
+        goberserk: {
+          aliases: [],
+          keywords: ['doom', 'space', 'marine'],
+          image: 'goberserk.png',
+          character: '',
+        },
+        godmode: {
+          aliases: ['invincible'],
+          keywords: ['doom', 'space', 'marine'],
+          image: 'godmode.png',
+          character: '',
+        },
+        hurtrealbad: {
+          aliases: ['ouch'],
+          keywords: ['doom', 'space', 'marine'],
+          image: 'hurtrealbad.png',
+          character: '😧',
+        },
+        rage1: {
+          aliases: [],
+          keywords: ['doom', 'space', 'marine'],
+          image: 'rage1.png',
+          character: '😠',
+        },
+        rage2: {
+          aliases: [],
+          keywords: ['doom', 'space', 'marine'],
+          image: 'rage2.png',
+          character: '😠',
+        },
+        rage3: {
+          aliases: [],
+          keywords: ['doom', 'space', 'marine'],
+          image: 'rage3.png',
+          character: '😠',
+        },
+        rage4: {
+          aliases: [],
+          keywords: ['doom', 'space', 'marine'],
+          image: 'rage4.png',
+          character: '😠',
+        },
+        suspect: {
+          aliases: ['suspicious'],
+          keywords: ['doom', 'space', 'marine'],
+          image: 'suspect.png',
+          character: '',
+        },
+      },
+    });
+  });
+};
